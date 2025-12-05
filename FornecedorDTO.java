2f7a9986842af25d75bd491a4a91b397643b79af
Luisa Pimentel <lupcouto.2006@gmail.com>
2025-12-05 08:49:33 -0300

    Delete FornecedorDTO.java

--- FornecedorDTO.java
+++
@@ -1,9 +0,0 @@
-package br.unitins.topicos1.prancha.dto;
-import jakarta.validation.constraints.NotNull;
-
-public record FornecedorDTO(
-    
-    @NotNull(message = "O cnpj é obrigatório")
-    String cnpj
-    
-) {}
