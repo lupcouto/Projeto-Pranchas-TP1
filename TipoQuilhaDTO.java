977db5313b89c77fda424d39d404e767534db757
Luisa Pimentel <lupcouto.2006@gmail.com>
2025-12-05 08:59:03 -0300

    Delete TipoQuilhaDTO.java

--- TipoQuilhaDTO.java
+++
@@ -1,9 +0,0 @@
-package br.unitins.topicos1.prancha.dto;
-import jakarta.validation.constraints.NotNull;
-
-public record TipoQuilhaDTO (
-    
-    @NotNull(message = "O nome é obrigatório")
-    String nome
-    
-) {}
